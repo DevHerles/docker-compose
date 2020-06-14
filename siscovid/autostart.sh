8708508cfe98a0480a175dae310dadf40686c82d
HerlesINC <herles.incalla@gmail.com>
2020-06-13 19:21:27 -0500

    Improve docker projects

--- siscovid/autostart.sh
+++ siscovid/autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /
 #/usr/bin/python /usr/bin/odoo --load=web,mail_tracking,mail_sendgrid
-#/usr/bin/python /usr/bin/odoo -u oehealth_samu -d DOCKER_DB_SAMU &
-/usr/bin/python /usr/bin/odoo
+/usr/bin/python /usr/bin/odoo -u saludtrabajo -d DOCKER_SALUDTRABAJO &
+#/usr/bin/python /usr/bin/odoo
 tail -F -n0 /etc/hosts
